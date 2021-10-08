70dbb8d3392423f5b43410932b6e291c4bef5daa
Rian Negreiros <riannegreiros@gmail.com>
2021-10-07 21:19:06 -0300

    fix: return errors

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,15 +4,21 @@ import userService from '../services/userService'
 export const register = async (request: Request, response: Response) => {
   const { username, email, password, confirmPassword } = request.body
 
-  const user = await userService.saveUser({ username, email, password, confirmPassword });
-
-  return response.json(user).status(201)
+  try {
+    const user = await userService.saveUser({ username, email, password, confirmPassword });
+    return response.json(user).status(201)
+  } catch (error) {
+    return response.status(400).json(error)
+  }
 }
 
 export const login = async (request: Request, response: Response) => {
   const { email, password } = request.body
 
-  const user = await userService.authenticateUser({email, password})
-
-  return response.json(user).status(200)
+  try {
+    const user = await userService.authenticateUser({ email, password })
+    return response.json(user).status(200)
+  } catch (error) {
+    return response.status(400).json(error)
+  }
 }
