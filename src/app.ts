977fb4f4d7355253466b368b86140aa708c662a6
Rian Negreiros <riannegreiros@gmail.com>
2021-10-24 17:33:00 -0300

    feat: add swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
 import todoRoutes from './routes/todo.routes'
 import userRoutes from './routes/user.routes'
+import swaggerFile from '../swagger.json'
 import { dbConnection } from './data/database/dbConnection'
 
 export const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use("api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(userRoutes)
 app.use(todoRoutes)
 
