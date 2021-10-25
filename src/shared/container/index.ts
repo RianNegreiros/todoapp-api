acdac00ad0444ebfa88cc2fe53d6cf3a34e6ca58
Rian Negreiros <riannegreiros@gmail.com>
2021-10-25 15:01:14 -0300

    feat: add tsyringe singleton user repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe'
 import { ITodoRepository } from '../../data/repositories/ITodoRepository'
+import { IUserRepository } from '../../data/repositories/IUserRepository'
 import { TodoRepository } from '../../data/repositories/TodoRepository'
+import { UserRepository } from '../../data/repositories/UserRepository'
 
 container.registerSingleton<ITodoRepository>(
   "TodoRespository",
   TodoRepository
+)
+
+container.registerSingleton<IUserRepository>(
+  "UserRepository",
+  UserRepository
 )
