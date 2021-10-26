d97ffe56e4b20fa4d46906978722609e5d8c276d
Rian Negreiros <riannegreiros@gmail.com>
2021-10-26 10:34:13 -0300

    chore: change jest config

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ export default {
   // automock: false,
 
   // Stop running tests after `n` failures
-  // bail: 0,
+   bail: true,
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "C:\\Users\\riann\\AppData\\Local\\Temp\\jest",
@@ -146,7 +146,7 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/**/*spec.ts"],
+  testMatch: ["**/*spec.ts"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
