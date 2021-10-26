7098325038abd15e8a12a40c67d92487363cb607
Rian Negreiros <riannegreiros@gmail.com>
2021-10-26 17:37:14 -0300

    refactor: move app file to http folder

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,9 @@
 import { container } from 'tsyringe'
-import { ITodoRepository } from '@/modules/todos/repositories/ITodoRepository'
-import { IUserRepository } from '@/modules/users/repositories/IUserRepository'
-import { TodoRepository } from '@/modules/todos/repositories/TodoRepository'
-import { UserRepository } from '@/modules/users/repositories/UserRepository'
+import { ITodoRepository } from '@modules/todos/repositories/ITodoRepository'
+import { TodoRepository } from '@modules/todos/repositories/TodoRepository'
+import { IUserRepository } from '@modules/users/infra/typeorm/repositories/IUserRepository'
+import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRepository'
+
 
 container.registerSingleton<ITodoRepository>('TodoRespository', TodoRepository)
 
