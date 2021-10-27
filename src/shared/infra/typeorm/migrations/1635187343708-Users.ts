4896400e87d7db5e67657c3719f59da3221a4a17
Rian Negreiros <riannegreiros@gmail.com>
2021-10-27 19:45:03 -0300

    refactor: add foreign key to user migration

--- src/shared/infra/typeorm/migrations/1635187343708-Users.ts
+++ src/shared/infra/typeorm/migrations/1635187343708-Users.ts
@@ -1,43 +1,49 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class Users1635187343708 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKUserTodos',
+            referencedTableName: "todos",
+            referencedColumnNames: ["id"],
+            columnNames: ["todo_id"],
+          },
+        ],
+      })
+    )
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-        await queryRunner.createTable(
-            new Table({
-                name: 'users',
-                columns: [
-                    {
-                        name: 'id',
-                        type: 'uuid',
-                        isPrimary: true
-                    },
-                    {
-                        name: 'username',
-                        type: 'varchar'
-                    },
-                    {
-                        name: 'email',
-                        type: 'varchar',
-                        isUnique: true
-                    },
-                    {
-                        name: 'password',
-                        type: 'varchar'
-                    },
-                    {
-                        name: 'created_at',
-                        type: 'timestamp',
-                        default: 'now()'
-                    },
-                ]
-            })
-        )
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('users')
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
 }
