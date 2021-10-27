9f390b2e41997c463aa8d9c849d28b150ddb3db7
Rian Negreiros <riannegreiros@gmail.com>
2021-10-27 11:27:32 -0300

    chore: gitingore env file

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,6 @@
 export default {
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || '5h5h55h5hNGOORG=RGP=R',
+  jwtSecret: process.env.JWT_SECRET || 'a4c888b5c978c6ea70b14a5d3c414eb7',
+  refresh_secret_token: "399e600ff5d1f1acea6d0897ef4b0b42",
+  expires_In_token: "15m"
 }
