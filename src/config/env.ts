046869f7d743240df0586e3876c17f68d6373efa
Rian Negreiros <riannegreiros@gmail.com>
2021-10-27 11:59:24 -0300

    refactor: change env file to auth file

--- src/config/env.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'a4c888b5c978c6ea70b14a5d3c414eb7',
-  refresh_secret_token: "399e600ff5d1f1acea6d0897ef4b0b42",
-  expires_In_token: "15m"
-}
