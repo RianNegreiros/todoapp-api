48852f70b2b8aac51244c54b796ff435c5861a88
Rian Negreiros <riannegreiros@gmail.com>
2021-10-30 16:15:32 -0300

    refactor: remove dist folder

--- dist/shared/container/providers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _DateProvider = require("./DateProvider/DateProvider");
-
-_tsyringe.container.registerSingleton('DateProvider', _DateProvider.DateProvider);
