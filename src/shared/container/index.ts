67314c31fb213134de394b5c4ce5cb9205158118
Rian Negreiros <riannegreiros@gmail.com>
2021-10-30 16:31:16 -0300

    fix: import

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,10 @@
 import { container } from 'tsyringe'
 import { ITodoRepository } from '@modules/todos/repositories/ITodoRepository'
-import { TodoRepository } from '@modules/todos/repositories/TodoRepository'
 import { IUserRepository } from '@modules/users/repositories/IUserRepository'
 import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRepository'
 import { IUserTokensRepository } from '@modules/users/repositories/IUserTokensRepository'
 import { UserTokensRepository } from '@modules/users/infra/typeorm/repositories/UserTokensRespository'
+import { TodoRepository } from '@modules/todos/infra/typeorm/repositories/TodoRepository'
 
 
 container.registerSingleton<ITodoRepository>('TodoRespository', TodoRepository)
