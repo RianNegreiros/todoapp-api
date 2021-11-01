d0c1e8f4f2992fd4b06c3dd425463e0fb516664e
Rian Negreiros <riannegreiros@gmail.com>
2021-11-01 18:27:00 -0300

    refactor: change get todos routes to user routes

--- src/shared/infra/http/routes/todo.routes.ts
+++ src/shared/infra/http/routes/todo.routes.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express'
 import { CreateTodoController } from '@modules/todos/useCases/createTodo/CreateTodoController'
 import { DeleteTodoController } from '@modules/todos/useCases/deleteTodo/DeleteTodoController'
-import { GetAllTodosController } from '@modules/users/useCases/getAllTodos/GetAllTodosController'
-import { GetAllTodosCompletedController } from '@modules/users/useCases/getAllTodosCompleted/GetAllTodosCompletedController'
 import { SetTodoCompletedController } from '@modules/todos/useCases/setTodoCompleted/SetTodoCompletedController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 
@@ -11,17 +9,12 @@ const todoRoutes = Router()
 const createTodoController = new CreateTodoController()
 const deleteTodoController = new DeleteTodoController()
 const setTodoCompletedController = new SetTodoCompletedController()
-const getAllTodosCompletedController = new GetAllTodosCompletedController()
-const getAllTodosController = new GetAllTodosController()
+
 
 todoRoutes.post('/add', ensureAuthenticated, createTodoController.handle)
 
 todoRoutes.put('/setcompleted', setTodoCompletedController.handle)
 
 todoRoutes.delete('/delete', deleteTodoController.handle)
-
-todoRoutes.get('/alltodos', getAllTodosController.handle)
-
-todoRoutes.get('/completeds', getAllTodosCompletedController.handle)
 
 export { todoRoutes }
--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
-import { AuthenticateUserController } from '@modules/users/useCases/authenticateUser/AuthenticateUserController'
 import { CreateUserController } from '@modules/users/useCases/createUser/CreateUserController'
+import { GetAllTodosController } from '@modules/users/useCases/getAllTodos/GetAllTodosController'
+import { GetAllTodosCompletedController } from '@modules/users/useCases/getTodosCompleted/GeTodosCompletedController'
 
 const userRoutes = Router()
 
 const createUserController = new CreateUserController()
+const getAllTodosCompletedController = new GetAllTodosCompletedController()
+const getAllTodosController = new GetAllTodosController()
 
 userRoutes.post('/register', createUserController.handle)
+
+userRoutes.get('/alltodos', getAllTodosController.handle)
+
+userRoutes.get('/todoscompleteds', getAllTodosCompletedController.handle)
 
 export { userRoutes }
