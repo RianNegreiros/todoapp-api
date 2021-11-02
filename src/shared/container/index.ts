048719d850e6555c33ba3184e69d7c906910392a
Rian Negreiros <riannegreiros@gmail.com>
2021-11-02 01:42:45 -0300

    refactor: formmating

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,14 +1,22 @@
 import { container } from 'tsyringe'
 import { ITodoRepository } from '@modules/todos/repositories/ITodoRepository'
+import { TodoRepository } from '@modules/todos/infra/typeorm/repositories/TodoRepository'
 import { IUserRepository } from '@modules/users/repositories/IUserRepository'
 import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRepository'
 import { IUserTokensRepository } from '@modules/users/repositories/IUserTokensRepository'
 import { UserTokensRepository } from '@modules/users/infra/typeorm/repositories/UserTokensRespository'
-import { TodoRepository } from '@modules/todos/infra/typeorm/repositories/TodoRepository'
 
+container.registerSingleton<ITodoRepository>(
+  'TodoRespository', 
+  TodoRepository
+  )
 
-container.registerSingleton<ITodoRepository>('TodoRespository', TodoRepository)
+container.registerSingleton<IUserRepository>(
+  'UserRepository', 
+  UserRepository
+  )
 
-container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
-
-container.registerSingleton<IUserTokensRepository>('UserTokensRepository', UserTokensRepository)
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository
+)
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata'
 import express from 'express'
 import cors from 'cors'
 import swaggerUi from 'swagger-ui-express'
@@ -10,9 +11,9 @@ import '@shared/container'
 dbConnection()
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 app.use('api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use(cors())
 app.use(router)
 
 export { app }
