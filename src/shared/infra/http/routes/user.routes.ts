72a9891ac99d97f36874eeac4be985834d8c6ee8
Rian Negreiros <riannegreiros@gmail.com>
2021-11-03 02:59:45 -0300

    refactor: move get todos routes to todo routes

--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -1,18 +1,10 @@
 import { Router } from 'express'
 import { CreateUserController } from '@modules/users/useCases/createUser/CreateUserController'
-import { GetUserAllTodosController } from '@modules/todos/useCases/getUserAllTodos/GetUserAllTodosController'
-import { GetUserTodosCompletedController } from '@modules/todos/useCases/getUserTodosCompleted/GetUserTodosCompletedController'
 
 const userRoutes = Router()
 
 const createUserController = new CreateUserController()
-const getUserTodosCompletedController = new GetUserTodosCompletedController()
-const getAllTodosController = new GetUserAllTodosController()
 
 userRoutes.post('/register', createUserController.handle)
-
-userRoutes.get('/alltodos', getAllTodosController.handle)
-
-userRoutes.get('/todoscompleteds', getUserTodosCompletedController.handle)
 
 export { userRoutes }
