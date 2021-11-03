8997e6dc2af67c7af584426ebb0aa265d3a46c12
Rian Negreiros <riannegreiros@gmail.com>
2021-11-03 03:00:36 -0300

    refactor: change routes names

--- src/shared/infra/http/routes/todo.routes.ts
+++ src/shared/infra/http/routes/todo.routes.ts
@@ -3,18 +3,25 @@ import { CreateTodoController } from '@modules/todos/useCases/createTodo/CreateT
 import { DeleteTodoController } from '@modules/todos/useCases/deleteTodo/DeleteTodoController'
 import { SetTodoStatusController } from '@modules/todos/useCases/setTodoStatus/SetTodoStatusController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+import { GetUserTodosCompletedController } from '@modules/todos/useCases/getUserTodosCompleted/GetUserTodosCompletedController'
+import { GetUserAllTodosController } from '@modules/todos/useCases/getUserAllTodos/GetUserAllTodosController'
 
 const todoRoutes = Router()
 
 const createTodoController = new CreateTodoController()
 const deleteTodoController = new DeleteTodoController()
 const setTodoStatusController = new SetTodoStatusController()
+const getUserTodosCompletedController = new GetUserTodosCompletedController()
+const getAllTodosController = new GetUserAllTodosController()
 
+todoRoutes.post('/create', ensureAuthenticated, createTodoController.handle)
 
-todoRoutes.post('/add', ensureAuthenticated, createTodoController.handle)
-
-todoRoutes.put('/setcompleted', setTodoStatusController.handle)
+todoRoutes.put('/status', setTodoStatusController.handle)
 
 todoRoutes.delete('/delete', deleteTodoController.handle)
+
+todoRoutes.get('/all', getAllTodosController.handle)
+
+todoRoutes.get('/completeds', getUserTodosCompletedController.handle)
 
 export { todoRoutes }
