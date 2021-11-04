6c3799da934a1b40e9814d9e984177e95f059751
Rian Negreiros <riannegreiros@gmail.com>
2021-11-04 19:58:22 -0300

    refactor: make tests independent

--- tests/modules/users/usecases/createUser/CreateUserController.spec.ts
+++ tests/modules/users/usecases/createUser/CreateUserController.spec.ts
@@ -1,15 +1,16 @@
 import request from 'supertest'
 import { Connection, createConnection } from 'typeorm'
 import { app } from '@shared/infra/http/app'
+import { IRegisterUserRequest } from '@modules/users/dtos/IRegisterUserRequest'
 
 let connection: Connection
 describe('Create user controller', () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     connection = await createConnection()
     await connection.runMigrations()
   })
 
-  afterAll(async () => {
+  afterEach(async () => {
     await connection.dropDatabase()
     await connection.close()
   })
@@ -26,11 +27,24 @@ describe('Create user controller', () => {
   })
 
   it('Should return 400 if user registration fails', async () => {
-    const response = await request(app).post('/users/register').send({
+    const userData: IRegisterUserRequest = {
       username: 'registerUser',
       email: 'registerUser@mail.com',
       password: 'registerUSER123@',
       confirmPassword: 'registerUSER123@',
+    }
+    await request(app).post('/users/register').send({
+      username: userData.username,
+      email: userData.email,
+      password: userData.password,
+      confirmPassword: userData.confirmPassword,
+    })
+
+    const response = await request(app).post('/users/register').send({
+      username: userData.username,
+      email: userData.email,
+      password: userData.password,
+      confirmPassword: userData.confirmPassword,
     })
 
     expect(response.status).toBe(400)
