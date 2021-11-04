140946b8d6dd864e974653641301229c580aaed9
Rian Negreiros <riannegreiros@gmail.com>
2021-11-04 19:54:58 -0300

    refactor: use register user request interface on request body

--- src/modules/users/usecases/createUser/CreateUserController.ts
+++ src/modules/users/usecases/createUser/CreateUserController.ts
@@ -1,10 +1,11 @@
+import { IRegisterUserRequest } from '@modules/users/dtos/IRegisterUserRequest'
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 import { CreateUserUseCase } from './CreateUserUseCase'
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { username, email, password, confirmPassword } = request.body
+    const { username, email, password, confirmPassword }: IRegisterUserRequest = request.body
     const createUserUseCase = container.resolve(CreateUserUseCase)
 
     try {
