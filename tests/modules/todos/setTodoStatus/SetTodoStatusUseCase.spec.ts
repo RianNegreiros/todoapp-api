258931577be94c5f2d6116b46a7652d00982d1cf
Rian Negreiros <riannegreiros@gmail.com>
2021-11-05 04:09:27 -0300

    test: Should throws if todo is not found

--- tests/modules/todos/setTodoStatus/SetTodoStatusUseCase.spec.ts
+++ tests/modules/todos/setTodoStatus/SetTodoStatusUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { v4 as uuidV4 } from 'uuid'
 import { TodoRepositoryInMemory } from '@modules/todos/repositories/inMemory/TodoRepositoryInMemory'
 import { CreateTodoUseCase } from '@modules/todos/useCases/createTodo/CreateTodoUseCase'
 import { SetTodoStatusUseCase } from '@modules/todos/useCases/setTodoStatus/SetTodoStatusUseCase'
@@ -45,5 +46,25 @@ describe('Create Todo Use Case', () => {
 
     const todo = await todoRepositoryInMemory.findTodoById(createTodo.id)
     expect(todo.completed).toBeTruthy()
+  })
+
+  it('Should throws if todo is not found', async () => {
+    expect(async () => {
+      const userData: IRegisterUserRequest = {
+        username: 'setStatus',
+        email: 'setStatus@mail.com',
+        password: 'setSTATUS123@',
+        confirmPassword: 'setSTATUS123@',
+      }
+      await createUserUseCase.execute(userData)
+      const userCreated = await userRepositoryInMemory.findUserByEmail(
+        userData.email
+      )
+
+      await createTodoUseCase.execute(userCreated.id, 'new todo body')
+
+      const todoId = uuidV4()
+      await setTodoStatusUseCase.execute(todoId, true)
+    }).rejects.toThrow(new Error('Todo not found by this id'))
   })
 })
