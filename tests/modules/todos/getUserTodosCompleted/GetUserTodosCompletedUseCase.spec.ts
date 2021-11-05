9b790d4b8c68f3b8ca1dae6397e1483c610098bb
Rian Negreiros <riannegreiros@gmail.com>
2021-11-05 06:51:13 -0300

    test: Should be able to return a list of user todos completeds

---
+++ tests/modules/todos/getUserTodosCompleted/GetUserTodosCompletedUseCase.spec.ts
@@ -0,0 +1,52 @@
+import { v4 as uuidV4 } from 'uuid'
+import { TodoRepositoryInMemory } from '@modules/todos/repositories/inMemory/TodoRepositoryInMemory'
+import { CreateTodoUseCase } from '@modules/todos/useCases/createTodo/CreateTodoUseCase'
+import { IRegisterUserRequest } from '@modules/users/dtos/IRegisterUserRequest'
+import { UserRepositoryInMemory } from '@modules/users/repositories/inMemory/UserRepositoryInMemory'
+import { CreateUserUseCase } from '@modules/users/useCases/createUser/CreateUserUseCase'
+import { GetUserTodosCompletedUseCase } from '@modules/todos/useCases/getUserTodosCompleted/GetUserTodosCompletedUseCase'
+import { SetTodoStatusUseCase } from '@modules/todos/useCases/setTodoStatus/SetTodoStatusUseCase'
+
+let userRepositoryInMemory: UserRepositoryInMemory
+let todoRepositoryInMemory: TodoRepositoryInMemory
+
+let createUserUseCase: CreateUserUseCase
+let createTodoUseCase: CreateTodoUseCase
+let getUserTodosCompleted: GetUserTodosCompletedUseCase
+let setTodoStatusUseCase: SetTodoStatusUseCase
+describe('Get User Todos Completed Use Case', () => {
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory()
+    todoRepositoryInMemory = new TodoRepositoryInMemory()
+
+    createUserUseCase = new CreateUserUseCase(userRepositoryInMemory)
+    createTodoUseCase = new CreateTodoUseCase(
+      todoRepositoryInMemory,
+      userRepositoryInMemory
+    )
+
+    getUserTodosCompleted = new GetUserTodosCompletedUseCase(
+      userRepositoryInMemory,
+      todoRepositoryInMemory
+    )
+
+    setTodoStatusUseCase = new SetTodoStatusUseCase(todoRepositoryInMemory)
+  })
+
+  it('Should be able to return a list of user todos completeds', async () => {
+    const userData: IRegisterUserRequest = {
+      username: 'setStatus',
+      email: 'setStatus@mail.com',
+      password: 'setSTATUS123@',
+      confirmPassword: 'setSTATUS123@',
+    }
+    await createUserUseCase.execute(userData)
+    const user = await userRepositoryInMemory.findUserByEmail(userData.email)
+
+    const createTodo = await createTodoUseCase.execute(user.id, 'new todo body')
+    await setTodoStatusUseCase.execute(createTodo.id, true)
+
+    const completedTodos = await getUserTodosCompleted.execute(user.id)
+    expect(completedTodos.length).toBeGreaterThan(0)
+  })
+})
