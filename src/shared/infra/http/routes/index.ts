28a82fe4e77fdd6d041d10859c828f1ff604b1c7
Rian Negreiros <riannegreiros@gmail.com>
2021-11-07 00:08:56 -0300

    refactor: add refactors to routes index file

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { authenticateRoutes } from './authenticate.routes'
+import { passwordRoutes } from './password.routes'
 import { todoRoutes } from './todo.routes'
 import { userRoutes } from './user.routes'
 
@@ -8,5 +9,6 @@ const router = Router()
 router.use('/users', userRoutes)
 router.use('/todos', todoRoutes)
 router.use('/authentication', authenticateRoutes)
+router.use('/password', passwordRoutes)
 
 export { router }
